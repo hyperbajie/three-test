813875418fb5f4b6720a73a37ad5c9d8a74af179
zhangsen <maizhanwangle@163.com>
2022-03-30 11:45:53 +0800

    feat: temporary commit

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,10 +1,12 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
+const path = require("path");
 
 module.exports = merge(common, {
     mode: "development",
     devServer: {
-        port: 3000
+        port: 3000,
+        static: ['assets'],
     },
     devtool: 'inline-source-map',
 })
--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,5 +1,7 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = merge(common, {
     mode: 'production',
@@ -7,5 +9,14 @@ module.exports = merge(common, {
         hints: false,
         maxEntrypointSize: 512000,
         maxAssetSize: 512000
-    }
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: "assets"
+                }
+            ]
+        })
+    ]
 })
